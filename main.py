0363a9be23a7cfa355ed3c41f166094232534b7f
jedson Ferreira <jedazevedo@gmail.com>
2025-03-22 20:54:27 -0300

    Inserindo elementos post e bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,22 +7,21 @@ class Jogo():
         self.nome = nome
         self.categoria = categoria
         self.console = console
-jogo1 = Jogo("Super Mario", 'Açao', 'SNES')
-jogo2 = Jogo("Pokemom Gold", 'RPG', 'GBA')
-jogo3 = Jogo("Mortal Kombat", 'Luta', 'SNES')
+# jogo1 = Jogo("Super Mario", 'Açao', 'SNES')
+# jogo2 = Jogo("Pokemom Gold", 'RPG', 'GBA')
+# jogo3 = Jogo("Mortal Kombat", 'Luta', 'SNES')
+# jogo4 = Jogo("Elder Ring", 'RPG', 'PS4')
     
-lista =[jogo1, jogo2, jogo3]
+lista =[]
 
 @app.route('/')
 def inicio():
     return render_template('index.html', 
                            titulo='Jogos',
                            jogos = lista)
-428
-@app.route('/novo')
-def novo_jogo():
-    return render_template('novo.html')
-
+# @app.route('/novo')
+# def novo_jogo():
+#     return render_template('novo.html')
 
 @app.route('/criar', methods=['POST',])
 def criar():
@@ -31,9 +30,7 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('index.html', 
-                           titulo='Jogos',
-                           jogos = lista)
+    return redirect('/')
 
 
 app.run(debug=True)
