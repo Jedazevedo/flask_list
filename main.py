8699a845d91234c35596722ad311e7499fc1248b
jedson Ferreira <jedazevedo@gmail.com>
2025-03-23 17:49:19 -0300

    adicionando funcao de calculo de tugs e corrigindo regras de calculo de potencia de luz

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 from flask import Flask, render_template, request, redirect
-
+import math
 app = Flask(__name__)
 
 class Area():
-    def __init__(self, nome, area, perimetro, potencia_luz):
+    def __init__(self,
+                 nome: str,
+                 area: float,
+                 perimetro: float,
+                 potencia_luz: int, 
+                 quantidade_tugs: int):
         self.nome = nome
         self.area = area
         self.perimetro = perimetro
         self.potencia_luz = potencia_luz
-            
+        self.quantidade_tugs = quantidade_tugs
+        
+                
 lista =[]
-
+def potencia_iluminacao(area: float):
+    if area<10: 
+        potencia_luz = 100
+    elif area<14:
+        potencia_luz = 160
+    elif area<18:
+        potencia_luz = 220
+    elif area <22:
+        potencia_luz = 280
+    elif area <26:
+        potencia_luz = 340
+    elif area <30:
+        potencia_luz = 340
+    else:
+        potencia_luz = "alta"
+    return potencia_luz    
+def quantidade_tugs(area: float, perimetro: float):
+    quantidade_de_tugs = 0
+    if area<6:
+        quantidade_de_tugs = 1        
+    else:
+        quantidade_de_tugs = math.ceil(perimetro/5)
+    return quantidade_de_tugs   
+ 
 @app.route('/')
 def inicio():
     return render_template('index.html', 
@@ -25,19 +55,9 @@ def criar():
     
     area = float(area)
     perimetro = float(perimetro)
-    
-    if area <=6: 
-        potencia_iluminacao = 100
-    elif area <=10:
-        potencia_iluminacao = 160
-    elif area <=14:
-        potencia_iluminacao = 220
-    elif area <=18:
-        potencia_iluminacao = 280
-    elif area <=22:
-        potencia_iluminacao = 340
+    tugs = 0    
 
-    area = Area(nome, area, perimetro, potencia_iluminacao)
+    area = Area(nome, area, perimetro, potencia_iluminacao(area), quantidade_tugs(area, perimetro))
     lista.append(area)
     return redirect('/')
 
