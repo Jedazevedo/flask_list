8e7d5a0bb0eae7ae917d70af7e0f0e2f68b0416b
jedson Ferreira <jedazevedo@gmail.com>
2025-03-22 23:15:39 -0300

    inserindo elementos de potencia de iluminação

--- main.py
+++ main.py
@@ -2,34 +2,43 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-class Jogo():
-    def __init__(self, nome, categoria, console):
+class Area():
+    def __init__(self, nome, area, perimetro, potencia_luz):
         self.nome = nome
-        self.categoria = categoria
-        self.console = console
-# jogo1 = Jogo("Super Mario", 'Açao', 'SNES')
-# jogo2 = Jogo("Pokemom Gold", 'RPG', 'GBA')
-# jogo3 = Jogo("Mortal Kombat", 'Luta', 'SNES')
-# jogo4 = Jogo("Elder Ring", 'RPG', 'PS4')
-    
+        self.area = area
+        self.perimetro = perimetro
+        self.potencia_luz = potencia_luz
+            
 lista =[]
 
 @app.route('/')
 def inicio():
     return render_template('index.html', 
-                           titulo='Jogos',
-                           jogos = lista)
-# @app.route('/novo')
-# def novo_jogo():
-#     return render_template('novo.html')
+                           titulo='Dependências',
+                           areas = lista)
 
 @app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
+    area = request.form['area']
+    perimetro = request.form['perimetro']
+    
+    area = float(area)
+    perimetro = float(perimetro)
+    
+    if area <=6: 
+        potencia_iluminacao = 100
+    elif area <=10:
+        potencia_iluminacao = 160
+    elif area <=14:
+        potencia_iluminacao = 220
+    elif area <=18:
+        potencia_iluminacao = 280
+    elif area <=22:
+        potencia_iluminacao = 340
+
+    area = Area(nome, area, perimetro, potencia_iluminacao)
+    lista.append(area)
     return redirect('/')
 
 
